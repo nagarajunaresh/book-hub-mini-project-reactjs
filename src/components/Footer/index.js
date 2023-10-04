e54fd42ade21301c0429a1155ed8bc3ebf8154ce
root <nagarajunaresh83@gmail.com>
2023-10-04 02:08:14 +0000

    solution

---
+++ src/components/Footer/index.js
@@ -0,0 +1,78 @@
+import {
+  FaGoogle,
+  FaTwitter,
+  FaLinkedin,
+  FaGithub,
+  FaInstagram,
+  FaYoutube,
+} from 'react-icons/fa'
+import HeaderContext from '../../context/HeaderContext'
+import './index.css'
+
+const Footer = () => (
+  <HeaderContext.Consumer>
+    {value => {
+      const {isDarkTheme} = value
+
+      const footerDarkText = isDarkTheme ? 'footer-dark-text' : ''
+
+      return (
+        <div className="footer-container">
+          <div className="icons-container">
+            <a
+              href="mailto:nagarajunaresh83@gmail.com"
+              className="footer-icon-button"
+            >
+              <FaGoogle className={`footer-icon ${footerDarkText}`} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/nagarajue/"
+              target="__blank"
+              rel="noopener noreferrer"
+              className="footer-icon-button"
+            >
+              <FaLinkedin className={`footer-icon ${footerDarkText}`} />
+            </a>
+            <a
+              href="https://github.com/nagarajunaresh"
+              target="__blank"
+              rel="noopener noreferrer"
+              className="footer-icon-button"
+            >
+              <FaGithub className={`footer-icon ${footerDarkText}`} />
+            </a>
+            <a
+              href="https://www.twitter.com"
+              target="__blank"
+              rel="noopener noreferrer"
+              className="footer-icon-button"
+            >
+              <FaTwitter className={`footer-icon ${footerDarkText}`} />
+            </a>
+            <a
+              href="https://www.instagram.com/nagaraju_naresh/"
+              target="__blank"
+              rel="noopener noreferrer"
+              type="button"
+              className="footer-icon-button"
+            >
+              <FaInstagram className={`footer-icon ${footerDarkText}`} />
+            </a>
+            <a
+              href="https://www.youtube.com/@Elavena_Nagaraju"
+              target="__blank"
+              rel="noopener noreferrer"
+              type="button"
+              className="footer-icon-button"
+            >
+              <FaYoutube className={`footer-icon ${footerDarkText}`} />
+            </a>
+          </div>
+          <p className={`contact-us-text ${footerDarkText}`}>Contact us</p>
+        </div>
+      )
+    }}
+  </HeaderContext.Consumer>
+)
+
+export default Footer
